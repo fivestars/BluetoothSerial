5ba80a60462e5030a2fd823ef2d7f12b3d1dd0aa
Darran Kelinske <darran.kelinske@gmail.com>
2020-02-26 08:43:48 -0600

    Lets connect listen

--- www/bluetoothSerial.js
+++ www/bluetoothSerial.js
@@ -28,19 +28,8 @@ module.exports = {
     },
 
     // calls the success callback when new data is available with an ArrayBuffer
-    subscribeRawData: function (success, failure) {
-
-        successWrapper = function(data) {
-            // Windows Phone flattens an array of one into a number which
-            // breaks the API. Stuff it back into an ArrayBuffer.
-            if (typeof data === 'number') {
-                var a = new Uint8Array(1);
-                a[0] = data;
-                data = a.buffer;
-            }
-            success(data);
-        };
-        cordova.exec(successWrapper, failure, "BluetoothSerial", "subscribeRaw", []);
+    listen: function (success, failure) {
+        cordova.exec(success, failure, "BluetoothSerial", "listen", []);
     },
 
     discoverUnpaired: function (success, failure) {
